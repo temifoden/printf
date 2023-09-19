6ff2da99b9cba83dc43b2fdfcfc2062e187edde7
Temi Afolabi <linuxmathew1245@gmail.com>
2023-09-19 08:15:38 +0100

    update source file for task-0

--- 0-print.c
+++ 0-print.c
@@ -1,12 +1,72 @@
 #include "main.h"
+#include <stdarg.h>
 
 /**
  * _printf - function entry
  * @format: first parameter
- * 
+ *
  * Return: an integer value
 */
 
-int _printf(const char *format, ...){
-
+int _printf(const char *format, ...)
+{
+    int count;
+    va_list my_list;
+    va_start(my_list, format);
+    
+    for (count = 0; *format; count++)
+    {
+        char c;
+        char *str;
+        
+        if (*format == '%'){
+            format++;
+            switch (*format)
+            {
+            case 'c':
+                c = va_arg(my_list, int);
+                pchar(c);
+                count++;
+                break;
+            case 's':
+                str = va_arg(my_list, char*);
+                while (*str)
+                {
+                    pchar(*str);
+                    str++;
+                    count++;
+                }
+                break;
+            case 'd':
+                c = va_arg(my_list, int);
+                if (c < 10){
+                    pchar(c);
+                    count++;
+                } else if(c >= 10){
+                    pchar(c / 10);
+                    count++;
+                    pchar(c % 10);
+                    count++;
+                }            
+                break;
+            case '%':
+                pchar('%');
+                count++;
+                break;
+            default:
+                pchar('%');
+                count++;
+                pchar(*format);
+                count++;
+                break;
+            }
+        }
+        else{
+            pchar(*format);
+            count++;
+        }
+        format++;
+    }
+    va_end(my_list);
+    return (count);
 }
