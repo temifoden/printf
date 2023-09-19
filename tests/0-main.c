6a46bdb9e1ff4e213e059c432d649973ae61fc5e
personal <omiwoletemitope@gmail.com>
2023-09-19 21:55:11 +0100

    work on restructuring

--- tests/0-main.c
+++ tests/0-main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "../header/main.h"
 #include <stdio.h>
 
 
@@ -6,7 +6,7 @@ int main(void)
 {
     int len;
 
-    len = _printf("Hello, %s!\n", "Holberton");
+    len = _printf("Hello, %s!\n", "School");
     printf("Length: %d\n", len);
 
     len = _printf("Character: %c\n", 'H');
